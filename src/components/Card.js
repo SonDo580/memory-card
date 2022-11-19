c118e1517a1f6b641e96fe71c9e6c29d268ae3ae
Son Do <dominhson580@gmail.com>
2022-11-19 16:10:07 +0700

    Create a Card component

---
+++ src/components/Card.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Card(props) {
+  const { card } = props;
+
+  return (
+    <div key={card.id}>
+      <img src={card.image} alt={`character ${card.id}`} />
+      <p>Charater {card.id}</p>
+    </div>
+  );
+}
+
+export default Card;
