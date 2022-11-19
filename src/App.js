fef0c7734d25ea8f1d4d6134815b396a26d54392
Son Do <dominhson580@gmail.com>
2022-11-19 15:29:50 +0700

    Define function to shuffle 1 array

--- src/App.js
+++ src/App.js
@@ -6,6 +6,25 @@ for (let i = 1; i <= 12; i++) {
   cards.push({ id: i, image: url });
 }
 
+const shuffleArray = (arr) => {
+  let currentIndex = arr.length - 1;
+  let randomIndex;
+
+  while (currentIndex !== 0) {
+    // Pick a remaining element and swap it with the current element
+    randomIndex = Math.floor(Math.random() * currentIndex);
+
+    [arr[currentIndex], arr[randomIndex]] = [
+      arr[randomIndex],
+      arr[currentIndex],
+    ]; // Use destructuring assignment
+
+    currentIndex--;
+  }
+
+  return arr;
+};
+
 function App() {
   const [score, setScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
@@ -20,6 +39,7 @@ function App() {
         <span>Score: {score}</span>
         <span>Best score: {bestScore}</span>
       </p>
+      <div className="cards"></div>
     </div>
   );
 }
